0c01a869028f37769980bc3c6b1fa71081674756
mdodsworth <dodster.michael@googlemail.com>
2012-04-20 23:52:56 -0700

    initial

---
+++ src/main/java/benchmark/regex/RegexBenchmark.java
@@ -0,0 +1,97 @@
+package benchmark.regex;
+
+import com.google.caliper.Param;
+import com.google.caliper.Runner;
+import com.google.caliper.SimpleBenchmark;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.regex.Pattern;
+
+/**
+ * FIXME: mdodsworth - Document me!
+ *
+ * @author mdodsworth
+ */
+public class RegexBenchmark extends SimpleBenchmark {
+
+    @Param({"100", "1000"}) private int numberOfTokens;
+    @Param private Expression expression;
+    @Param private DataSource dataSource;
+    @Param private RegexImpl regexImpl;
+
+    private CharSequence[] tokens;
+
+    private static enum Expression {
+        EMAIL_ADDRESS("[a-z]*"),
+        PHONE_NUMBER("[0-5]{3}");
+
+        private final String expression;
+
+        private Expression(String expression) {
+            this.expression = expression;
+        }
+
+        public String getExpression() {
+            return expression;
+        }
+    }
+
+    private static enum DataSource {
+        RANDOM_ALPHABETIC {
+            @Override
+            public CharSequence nextToken() {
+                return RandomStringUtils.randomAlphabetic(10);
+            }
+        },
+        RANDOM_ALPHANUMERIC {
+            @Override
+            public CharSequence nextToken() {
+                return RandomStringUtils.randomAlphanumeric(10);
+            }
+        };
+
+        public abstract CharSequence nextToken();
+    }
+
+    private static enum RegexImpl {
+        JDK_REGEX {
+            private Pattern pattern;
+
+            @Override
+            public void initialize(Expression expression) {
+                pattern = Pattern.compile(expression.getExpression());
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence) {
+                return pattern.matcher(charSequence).matches();
+            }
+        };
+
+        public abstract void initialize(Expression expression);
+        public abstract boolean matches(CharSequence charSequence);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        tokens = new CharSequence[numberOfTokens];
+        for (int i = 0; i < numberOfTokens; i++) {
+            tokens[i] = dataSource.nextToken();
+        }
+
+        regexImpl.initialize(expression);
+    }
+
+    public void timeRegexExpressions(int numReps) {
+        for (int i = 0; i < numReps; i++) {
+            for (CharSequence token : tokens) {
+                regexImpl.matches(token);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Runner.main(RegexBenchmark.class, args);
+    }
+}
